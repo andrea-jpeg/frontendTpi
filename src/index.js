0b1f0b9821ccee208b424d2cce79bc746e2049d0
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2019-04-29 11:31:49 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import MainPage from "./pages/MainPage.js";
 
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <MainPage />;
 }
 
 const rootElement = document.getElementById("root");
