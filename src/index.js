5175b68bfa0a6bbdd275c7b297da576aa9abbf4d
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2019-04-29 11:53:48 +0200

    react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MainPage from "./pages/MainPage.js";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom"; //{}=variabile
 import "./styles.css";
 
 function App() {
-  return <MainPage />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={MainPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 const rootElement = document.getElementById("root");
