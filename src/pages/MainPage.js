7695bc28ca8a2812ca581ba966e234b0be465cfb
MichelePavo <michelepavo00@gmail.com>
2019-05-05 09:25:08 +0000

    m

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -176,6 +176,28 @@ class SignIn extends React.Component{
           >
             sing up
           </Button>
+           <div className={classes.root}>
+      <GridList className={classes.gridList} cols={2.5}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+
+    </div>
         </form>
       </Paper>
     </main>
