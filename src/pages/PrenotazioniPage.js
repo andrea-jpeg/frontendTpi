2da0723e1089ce41df65279dd00eb2a8271a42ee
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2019-05-05 13:31:05 +0000

    immagine Sfondo

--- src/pages/PrenotazioniPage.js
+++ src/pages/PrenotazioniPage.js
@@ -26,7 +26,8 @@ const styles = theme => ({
     display: "none"
   },
   root: {
-      backgroundImage:'https://i.pinimg.com/736x/43/f5/a2/43f5a2f1c7eebb1dadb1a597e748ba77.jpg',
+      backgroundImage: `url(https://i.pinimg.com/736x/43/f5/a2/43f5a2f1c7eebb1dadb1a597e748ba77.jpg)`,
+
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2
