029ede2d6dcaa998961d63bd07732ddabcb73a37
MichelePavo <michelepavo00@gmail.com>
2019-05-05 13:29:20 +0000

    g

--- src/pages/PrenotazioniPage.js
+++ src/pages/PrenotazioniPage.js
@@ -26,6 +26,7 @@ const styles = theme => ({
     display: "none"
   },
   root: {
+      backgroundImage:'https://i.pinimg.com/736x/43/f5/a2/43f5a2f1c7eebb1dadb1a597e748ba77.jpg',
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2
